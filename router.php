c5d2dc4dfa402274b11b800d7693a830e28d17ac
Carlos <carlos@youngmoney.com>
2026-01-28 08:28:21 -0500

    Adiciona rotas para servir arquivos da pasta Postback

--- router.php
+++ router.php
@@ -57,6 +57,40 @@ if (preg_match('/\.php$/', $path)) {
     }
 }
 
+// Rotas da pasta Postback (site de anúncios)
+if (preg_match('/^\/Postback\//', $path) || preg_match('/^\/postback\//', $path)) {
+    // Normalizar para Postback (com P maiúsculo)
+    $postbackPath = preg_replace('/^\/postback\//i', '/Postback/', $path);
+    $file = __DIR__ . $postbackPath;
+    
+    // Se for diretório, servir index.html
+    if (is_dir($file)) {
+        $file = rtrim($file, '/') . '/index.html';
+    }
+    
+    if (file_exists($file) && is_file($file)) {
+        $ext = pathinfo($file, PATHINFO_EXTENSION);
+        $mimeTypes = [
+            'html' => 'text/html; charset=UTF-8',
+            'css' => 'text/css',
+            'js' => 'application/javascript',
+            'json' => 'application/json',
+            'png' => 'image/png',
+            'jpg' => 'image/jpeg',
+            'jpeg' => 'image/jpeg',
+            'gif' => 'image/gif',
+            'svg' => 'image/svg+xml',
+            'woff' => 'font/woff',
+            'woff2' => 'font/woff2',
+        ];
+        if (isset($mimeTypes[$ext])) {
+            header('Content-Type: ' . $mimeTypes[$ext]);
+        }
+        readfile($file);
+        return true;
+    }
+}
+
 // Rota raiz - servir index.html do SPA
 if ($path === '/' || $path === '/index.html') {
     $file = __DIR__ . '/public/index.html';
